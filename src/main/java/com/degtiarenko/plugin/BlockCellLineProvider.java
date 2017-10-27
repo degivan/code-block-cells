e6210f3ceabbe103e3565b583ac94259a9e193fa
vanyadeg <id1997@yandex.ru>
2017-10-27 11:15:25 +0300

    added option for running cell from gutter

--- src/main/java/com/degtiarenko/plugin/BlockCellLineProvider.java
+++ src/main/java/com/degtiarenko/plugin/BlockCellLineProvider.java
@@ -3,7 +3,10 @@ package com.degtiarenko.plugin;
 import com.intellij.codeHighlighting.Pass;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.execution.lineMarker.LineMarkerActionWrapper;
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.colors.CodeInsightColors;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
@@ -11,6 +14,7 @@ import com.intellij.openapi.editor.markup.GutterIconRenderer;
 import com.intellij.openapi.editor.markup.SeparatorPlacement;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
+import com.intellij.util.Function;
 import com.jetbrains.python.psi.PyFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -21,7 +25,10 @@ import java.util.List;
 import static com.degtiarenko.plugin.CellUtil.BLOCK_CELL_SEPARATOR;
 
 public class BlockCellLineProvider implements LineMarkerProvider {
+    private static final String CELL_ACTION_ID = "PyExecuteCellAction";
+
     private EditorColorsManager colorsManager = EditorColorsManager.getInstance();
+    private AnAction action = ActionManager.getInstance().getAction(CELL_ACTION_ID);
 
     @Nullable
     @Override
@@ -42,19 +49,49 @@ public class BlockCellLineProvider implements LineMarkerProvider {
         return comment.getText().equals(BLOCK_CELL_SEPARATOR);
     }
 
-    // почти полностью скопирован с LineMarkerPass#createMethodSeparatorLineMarker
     @NotNull
-    private static LineMarkerInfo<PsiElement> createBlockCellLineMarker(@NotNull PsiElement element,
-                                                                        @NotNull EditorColorsManager colorsManager) {
-        LineMarkerInfo<PsiElement> info = new LineMarkerInfo<>(
+    private LineMarkerInfo<PsiElement> createBlockCellLineMarker(@NotNull PsiElement element,
+                                                                 @NotNull EditorColorsManager colorsManager) {
+        Function<PsiElement, String> tooltipProvider = psiElement -> action.toString();
+        final DefaultActionGroup actionGroup = new DefaultActionGroup();
+        actionGroup.add(new LineMarkerActionWrapper(element, action));
+
+        LineMarkerInfo<PsiElement> info = new LineMarkerInfo<PsiElement>(
                 element,
                 element.getTextRange(),
                 AllIcons.General.Run,
                 Pass.LINE_MARKERS,
+                tooltipProvider,
                 null,
-                null,
-                GutterIconRenderer.Alignment.RIGHT
-        );
+                GutterIconRenderer.Alignment.CENTER
+        ) {
+            @Override
+            public GutterIconRenderer createGutterRenderer() {
+                return new LineMarkerGutterIconRenderer<PsiElement>(this) {
+                    @Override
+                    public AnAction getClickAction() {
+                        return new AnAction() {
+                            @Override
+                            public void actionPerformed(AnActionEvent e) {
+                                Editor editor = CommonDataKeys.EDITOR.getData(e.getDataContext());
+                                editor.getCaretModel().moveToOffset(element.getTextOffset());
+                                action.actionPerformed(e);
+                            }
+                        };
+                    }
+
+                    @Override
+                    public boolean isNavigateAction() {
+                        return true;
+                    }
+
+                    @Override
+                    public ActionGroup getPopupMenuActions() {
+                        return null;
+                    }
+                };
+            }
+        };
         EditorColorsScheme scheme = colorsManager.getGlobalScheme();
         info.separatorColor = scheme.getColor(CodeInsightColors.METHOD_SEPARATORS_COLOR);
         info.separatorPlacement = SeparatorPlacement.TOP;
