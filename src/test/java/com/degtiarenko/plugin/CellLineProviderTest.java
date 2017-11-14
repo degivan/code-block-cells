65c28f0678e2d1e4d51d2f15a2ce3c9225e1258c
vanyadeg <id1997@yandex.ru>
2017-11-14 13:02:31 +0300

    more tests for line provider

--- src/test/java/com/degtiarenko/plugin/CellLineProviderTest.java
+++ src/test/java/com/degtiarenko/plugin/CellLineProviderTest.java
@@ -1,18 +1,45 @@
 package com.degtiarenko.plugin;
 
+import com.intellij.codeInsight.daemon.GutterMark;
+import com.intellij.codeInsight.daemon.LineMarkerInfo;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.editor.markup.GutterIconRenderer;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 
 import java.io.File;
+import java.util.List;
 
 public class CellLineProviderTest extends LightPlatformCodeInsightFixtureTestCase {
 
-    public void test() {
+    public void test_pyExtension_noLineMarkerInfo() {
         PsiFile file = myFixture.configureByFile("spam.py");
         CellLineProvider lineProvider = new CellLineProvider();
+        for (PsiElement elem: file.getChildren()) {
+            LineMarkerInfo lineMarkerInfo = lineProvider.getLineMarkerInfo(elem);
+            assertNull("Line provider returned not null", lineMarkerInfo);
+        }
+    }
 
-        assertNull("Line provider should not return anything",
-                lineProvider.getLineMarkerInfo(file.getFirstChild()));
+    public void test_ccpyExtensionNoComment_hasLineMarkerInfo() {
+        PsiFile file = myFixture.configureByFile("ham.ccpy");
+        CellLineProvider lineProvider = new CellLineProvider();
+
+        LineMarkerInfo lineMarkerInfo = lineProvider.getLineMarkerInfo(file.getFirstChild());
+
+        assertNotNull("Line provider returned null", lineMarkerInfo);
+    }
+
+    public void test_ccpyExtensionWithComment_correctGutters() {
+        myFixture.configureByFile("ham_with_comment.ccpy");
+        List<GutterMark> marks = myFixture.findAllGutters();
+
+        assertEquals(2, marks.size());
+
+        GutterIconRenderer mark = (GutterIconRenderer)marks.get(0);
+        AnAction clickAction = mark.getClickAction();
+        assertNotNull(clickAction);
     }
 
     @Override
