aed7902c84a9e5e979b291cb36beb5f96f8fa300
vanyadeg <id1997@yandex.ru>
2017-11-16 19:47:15 +0300

    more cell util tests

--- src/test/java/com/degtiarenko/plugin/CellUtilTest.java
+++ src/test/java/com/degtiarenko/plugin/CellUtilTest.java
@@ -8,8 +8,7 @@ import com.intellij.psi.util.PsiTreeUtil;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.degtiarenko.plugin.CellUtil.getCellStart;
-import static com.degtiarenko.plugin.CellUtil.isFileOfGoodType;
+import static com.degtiarenko.plugin.CellUtil.*;
 import static com.intellij.psi.util.PsiTreeUtil.findChildrenOfType;
 import static com.intellij.psi.util.PsiTreeUtil.nextLeaf;
 
@@ -30,7 +29,7 @@ public class CellUtilTest extends BaseTestCase {
         PsiComment comment = comments.get(0);
         PsiElement nextTopLevel = nextLeaf(nextLeaf(comment));
 
-        assertTrue(BROKEN_TEST_MSG, comment.getText().equals(CellUtil.BLOCK_CELL_SEPARATOR));
+        assertTrue(BROKEN_TEST_MSG, comment.getText().equals(BLOCK_CELL_SEPARATOR));
 
         testGetCellStart(nextTopLevel, comment.getNextSibling());
     }
@@ -41,7 +40,7 @@ public class CellUtilTest extends BaseTestCase {
         PsiComment comment = comments.get(0);
         PsiElement nonTopLevel = PsiTreeUtil.getDeepestLast(file);
 
-        assertTrue(BROKEN_TEST_MSG, comment.getText().equals(CellUtil.BLOCK_CELL_SEPARATOR));
+        assertTrue(BROKEN_TEST_MSG, comment.getText().equals(BLOCK_CELL_SEPARATOR));
 
         testGetCellStart(nonTopLevel, comment.getNextSibling());
     }
@@ -49,6 +48,29 @@ public class CellUtilTest extends BaseTestCase {
     private void testGetCellStart(PsiElement caretElem, PsiElement expected) {
         PsiElement cellStart = getCellStart(caretElem);
         assertEquals("Cell start is wrong", expected, cellStart);
+    }
+
+    public void testGetCodeInCell_fileFirstChild_returnTillFirstComment() {
+        PsiFile file = myFixture.configureByFile(CCPY_WITH_COMMENT);
+        PsiElement elem = file.getFirstChild();
+        String expectedCode = file.getText().split(BLOCK_CELL_SEPARATOR)[0];
+
+        testGetCodeInCell(elem, expectedCode);
+    }
+
+
+    public void testGetCodeInCell_singleComment_returnSinceCommentTillEnd() {
+        PsiFile file = myFixture.configureByFile(CCPY_WITH_COMMENT);
+        PsiElement elem = PsiTreeUtil.findChildOfType(file, PsiComment.class)
+                            .getNextSibling();
+        String expectedCode = file.getText().split(BLOCK_CELL_SEPARATOR)[1];
+
+        testGetCodeInCell(elem, expectedCode);
+    }
+
+    private void testGetCodeInCell(PsiElement startElem, String expectedCode) {
+        String code = getCodeInCell(startElem);
+        assertEquals(expectedCode, code);
     }
 
     public void testIsFileOfGoodType_pyExtension_returnFalse() {
